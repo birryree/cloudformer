e0ceb5280b1373cb1648244901d79a5ae8eb6056
William Lee <wlee@leaf.me>
2015-02-23 11:42:47 -0500

    Changed SNS to publish to SQS and added a policy to allow that behavior

--- core/queues.py
+++ core/queues.py
@@ -11,27 +11,18 @@ from troposphere.autoscaling import EC2_INSTANCE_TERMINATE
 import troposphere.cloudwatch as cloudwatch
 import troposphere.ec2 as ec2
 import troposphere.sns as sns
-import troposphere.sqs as sqs
 import troposphere.iam as iam
+from troposphere.sqs import QueuePolicy, Queue
 
 import config as cfn
 from config import CIDR_PREFIX, VPC_NAME, CLOUDNAME, CLOUDENV, ASSUME_ROLE_POLICY, template
 from config import USE_PRIVATE_SUBNETS, DEFAULT_ROUTE
 
 def emit_configuration():
-    babysitter_email_param = template.add_parameter(
-        Parameter(
-            'BabysitterAlarmEmail',
-            Default='wlee@leaf.me',
-            Description='Email address to notify if tehre are issues in the babysitter queue',
-            Type='String'
-        )
-    )
-
-   # Build an SQS queue for the babysitter
+    # Build an SQS queue for the babysitter
     queue_name = '_'.join(['chef-deregistration', CLOUDNAME, CLOUDENV])
     queue = template.add_resource(
-        sqs.Queue(
+        Queue(
             cfn.sanitize_id(queue_name),
             VisibilityTimeout=60,
             MessageRetentionPeriod=1209600,
@@ -47,8 +38,8 @@ def emit_configuration():
             TopicName=queue_name,
             Subscription=[
                 sns.Subscription(
-                    Endpoint=Ref(babysitter_email_param),
-                    Protocol='email'
+                    Endpoint=GetAtt(queue, "Arn"),
+                    Protocol='sqs'
                 ),
             ],
             DependsOn=queue.title
@@ -72,10 +63,37 @@ def emit_configuration():
             EvaluationPeriods='1',
             Threshold='200',
             ComparisonOperator='GreaterThanThreshold',
-            AlarmActions=[Ref(alert_topic), ],
-            InsufficientDataActions=[Ref(alert_topic), ],
+            #AlarmActions=[Ref(alert_topic), ],
+            #InsufficientDataActions=[Ref(alert_topic), ],
             DependsOn=alert_topic.title
         ),
+    )
+
+    queue_policy = {
+        "Version": "2012-10-17",
+        "Id": "BabysitterSNSPublicationPolicy",
+        "Statement": {
+            "Sid":"AllowSNSPublishing",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": Ref("AWS::AccountId")
+            },
+            "Action": "sqs:SendMessage",
+            "Resource": GetAtt(queue, "Arn"),
+            "Condition": {
+                "ArnEquals": {"aws:SourceArn": Ref(alert_topic)}
+            }
+        }
+    }
+
+    # Publish all events from SNS to the Queue
+    template.add_resource(
+        QueuePolicy(
+            "BabysitterPublishSNStoSQSPolicy",
+            Queues=[Ref(queue)],
+            PolicyDocument=queue_policy,
+            DependsOn=queue.title
+        )
     )
 
     cfn.alert_topic = alert_topic
